27a0879a465516c674b65739074cd56a58f759e7
Yu-Jie Lin <livibetter@gmail.com>
2010-02-01 20:20:36 +0800

    Added Disqus widgets

--- src/lso/ui.py
+++ src/lso/ui.py
@@ -9,18 +9,18 @@ def render_write(tmpl_values, tmpl_name, request=None, response=None):
   # A helper function to set up some common stuff, render, then write to client
   if 'HEAD' not in tmpl_values:
     tmpl_values['HEAD'] = ''
-  tmpl_values['HEAD'] += '  <link href="/css/main.css?r=5" type="text/css" rel="stylesheet"/>\n'
+  tmpl_values['HEAD'] += '  <link href="/css/main.css?r=6" type="text/css" rel="stylesheet"/>\n'
   tmpl_values['HEAD'] += '  <script src="http://www.google.com/jsapi" type="text/javascript"></script>\n'
-  tmpl_values['HEAD'] += '  <script src="/js/main.js" type="text/javascript"></script>\n'
+  tmpl_values['HEAD'] += '  <script src="/js/main.js?r=1" type="text/javascript"></script>\n'
   tmpl_values['HEAD'] += '  <script src="/js/jquery.easing.js" type="text/javascript"></script>\n'
   tmpl_values['HEAD'] += '  <script src="/js/humanmsg.js" type="text/javascript"></script>\n'
   tmpl_values['HEAD'] += '  <link href="/css/humanmsg.css" type="text/css" rel="stylesheet"/>\n'
   if request:
     style = request.COOKIES.get('style', '')
     if style and style in config.KEYS_VALID_STYLES:
-      tmpl_values['HEAD'] += '  <link href="/css/main-%s.css" type="text/css" rel="stylesheet"/>\n' % style
+      tmpl_values['HEAD'] += '  <link href="/css/main-%s.css?r=1" type="text/css" rel="stylesheet"/>\n' % style
     else:
-      tmpl_values['HEAD'] += '  <link href="/css/main-%s.css" type="text/css" rel="stylesheet"/>\n' % config.DEFAULT_STYLE
+      tmpl_values['HEAD'] += '  <link href="/css/main-%s.css?r=1" type="text/css" rel="stylesheet"/>\n' % config.DEFAULT_STYLE
     if style == 'light':
       tmpl_values['reCAPTCHA_THEME'] = '<script type="text/javascript">var RecaptchaOptions = {theme: "white"};</script>'
     else:
