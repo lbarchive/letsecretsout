c7d5817859af795db7eaba6d71378d077553623e
Yu-Jie Lin <livibetter@gmail.com>
2010-02-04 15:59:44 +0800

    Added Traditional Chinese

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -3,11 +3,12 @@ USE_I18N = True
 # Valid languages
 LANGUAGES = (
     # 'en', 'zh_TW' should match the directories in conf/locale/*
-    ('en', _('English')),  
+    ('en-us', _('US English')),
+    ('zh-tw', _('Traditional Chinese')),
     )
 
 # This is a default language
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'en-us'
 
 # Custom template tags
 INSTALLED_APPS = (
